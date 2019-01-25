971fc33ed8a649d2eac451568783b6712affc170
Carlos Avila Aguilar <krloz1003@gmail.com>
2019-01-24 18:58:27 -0600

    Agregando comentarios a las rutas de registro de modulo

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,28 @@
 <?php
 use Illuminate\Http\Request;
+// Registro de modulo autorizado para solicitud de NUC
 Route::get('/regmod1', function () {
+    /**
+     * Para poder ingresar a la ruta de alta de modulo el proyecto no debera
+     * contener dentro del .env la variable CLAVE, ya que esta es asignada al
+     * momento de registrar el modulo ante el API_NUC.
+     * 
+     * Si la clave no existe nos mandara a la vista para poder registrar nuestro modulo.
+     * Si la clave ya existe nos redirecciona a nuestra página principal.
+     */
     if(env("CLAVE")==null){
         return view("fge_tok::login-nuc");
     }
     return Redirect::to('/');    
 });
+
+/**
+ * Cuando se registra por primera vez el módulo pregunta se si existe la clave dentro del .env
+ * si la encuentra lo redirecciona a la página principal del proyecto
+ * si la clave es null se recibe como parametro el prefix del modulo y lo agrega al
+ * .env con una variable llamada CLAVE = prefix(parametro).
+ */
+
 Route::get('regmod2/{name}', function ($name) {
     if(env("CLAVE")==null){
         $envFile = app()->environmentFilePath();
@@ -16,24 +33,44 @@ Route::get('regmod2/{name}', function ($name) {
     }
     return Redirect::to('/');
 });
+
+/**
+ * Rutas para verifica NUC estas van direccionadas al mismo controlador dentro
+ * del paquete fge.nucc.
+ */
+
 Route::get('cnucl', function () {
     if(env("CLAVE")==null){
         return Redirect::to('fge_tok/regmod1');
     }
     return view("fge_tok::form-cnucl");
 });
+
+/**
+ * Mismo proceso que la ruta anterior.
+ */
 Route::get('cnucr', function () {
     if(env("CLAVE")==null){
         return Redirect::to('fge_tok/regmod1');
     }
     return view("fge_tok::form-cnucr");
 });
+
+/**
+ * Habilita NUC
+ */
+
 Route::get('hnuc', function () {
     if(env("CLAVE")==null){
         return Redirect::to('fge_tok/regmod1');
     }
     return view("fge_tok::form-hnuc");
 });
+
+/**
+ * Generar NUC: Si existe .env(clave) nos manda a la vista para generar NUC.
+ * 
+ */
 Route::get('gnuc', function () {
     if(env("CLAVE")==null){
         return Redirect::to('fge_tok/regmod1');
--- views/login-nuc.blade.php
+++ views/login-nuc.blade.php
@@ -12,6 +12,15 @@
     </div>
 </div>
 <script>
+    /**
+      * 1. Al dar click en el botón Entrar obtiene los datos de "nombre" y lo asigna a un arreglo llamado data
+      * 2. Envía el arreglo a la ruta fge-tok/regmod en formato json para registrar el modulo en nuestra BD.
+      * 3. Si fue correcto nos envía a la vista fge_tok/regmod2 con un parametro (mensaje).
+      * 4. Si no manda una alerta con el mensaje del error.
+      */
+
+    
+
     document.getElementById("singin").onclick=()=>{
         const data={name:document.getElementById("nombre").value};
         fetch('{{env('URL_FGE-NUC')}}'+'/fge-tok/regmod',{
